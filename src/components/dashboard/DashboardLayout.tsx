07b364a4fe506badb742e152e661e5759690a066
MD AZFAR ALAM <azfaralam.ops@gmail.com>
2025-06-04 01:02:56 +0530

    start post option

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -59,7 +59,7 @@ const DashboardLayout = () => {
             <div className="max-w-7xl mx-auto px-4 py-8">
               <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                 <div className="lg:col-span-2 space-y-6">
-                  
+                  <StartPost />
                   <Feed />
                 </div>
                 <div className="space-y-6">
